85c688f29b7f4de94896ff9c5a755890b046cf1e
ThePBone <thebone.main@gmail.com>
2019-12-03 21:28:59 +0100

    Force locale to C default

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QLocale::setDefault(QLocale::c());
     QApplication::setStyle("fusion");
     w.show();
     return QApplication::exec();
