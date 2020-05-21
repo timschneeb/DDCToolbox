dea8a01c570d5095307244fc8457fa0183905df0
Tim Schneeberger <thebone.main@gmail.com>
2020-05-21 04:03:43 +0200

    Add travis CI deployment for debian packages

---
+++ build_deb_package.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#DEB build script for Travis-CI
+#DDCToolbox binary needs to be in working directory
+
+debname="ddctoolbox_"$base_version"."$TRAVIS_BUILD_NUMBER
+echo $debname
+mkdir $debname
+mkdir $debname"/DEBIAN"
+mkdir -p $debname"/usr/bin"
+mkdir -p $debname"/usr/share/applications"
+mkdir -p $debname"/usr/share/pixmaps"
+cp "DDCToolbox" $debname"/usr/bin/ddc-toolbox"
+
+cp "img/icon.png" $debname"/usr/share/pixmaps/ddc-toolbox.png"
+cp "LICENSE" $debname"/DEBIAN"
+
+cat <<EOT >> $debname"/usr/share/applications/ddc-toolbox.desktop"
+[Desktop Entry]
+Name=DDC Toolbox
+GenericName=DDC Editor
+Comment=Create and edit DDCs on Linux
+Keywords=editor
+Categories=AudioVideo;Audio;Editor
+Exec=ddc-toolbox
+Icon=/usr/share/pixmaps/ddc-toolbox.png
+StartupNotify=false
+Terminal=false
+Type=Application
+EOT
+
+cat <<EOT >> $debname"/DEBIAN/control"
+Package: ddc-toolbox
+Version: $base_version.$TRAVIS_BUILD_NUMBER
+Section: sound
+Priority: optional
+Architecture: amd64
+Depends: qtbase5-dev (>= 5.14), libqt5core5a (>= 5.14), libqt5widgets5 (>= 5.14), libqt5gui5 (>= 5.14), libqt5core5a (>= 5.14), libgl1-mesa-dev
+Maintainer: ThePBone <tim.schneeberger@gmail.com>
+Description: Create and edit DDCs on Linux
+Homepage: https://github.com/ThePBone/DDCToolbox
+EOT
+
+dpkg-deb --build $debname
