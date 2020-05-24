28cd3b4b4a5616285637311854b99a54849b7d18
Tim Schneeberger <thebone.main@gmail.com>
2020-05-24 02:54:56 +0200

    Update build_deb_package.sh

--- deployment/build_deb_package.sh
+++ deployment/build_deb_package.sh
@@ -34,7 +34,7 @@ Version: $base_version-$TRAVIS_BUILD_NUMBER
 Section: sound
 Priority: optional
 Architecture: amd64
-Depends: qtbase5-dev (>= 5.14), libqt5core5a (>= 5.14), libqt5widgets5 (>= 5.14), libqt5gui5 (>= 5.14), libqt5core5a (>= 5.14), libgl1-mesa-dev
+Depends: qtbase5-dev (>= 5.11), libqt5core5a (>= 5.11), libqt5widgets5 (>= 5.11), libqt5gui5 (>= 5.11), libqt5core5a (>= 5.11), libgl1-mesa-dev
 Maintainer: ThePBone <tim.schneeberger@gmail.com>
 Description: Create and edit DDCs on Linux
 Homepage: https://github.com/ThePBone/DDCToolbox
