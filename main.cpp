e895a2cd03c129fca3e65e79f7bde2d5f17f74fb
ThePBone <thebone.main@gmail.com>
2020-09-21 23:14:40 +0200

    Force light color scheme on all OSes

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include "mainwindow.h"
 #include <QApplication>
 
-using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     QLocale::setDefault(QLocale::c());
     QApplication::setStyle("fusion");
+    a.setPalette(qApp->style()->standardPalette());
     w.show();
 
     return QApplication::exec();
