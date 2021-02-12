802b3f1615fed650ffcf0bb270c8f4132442db81
Tim Schneeberger <tim.schneeberger@outlook.de>
2021-02-12 16:10:27 +0100

    Add automated linux64 CI builds

--- deployment/build_deb_package.sh
+++ deployment/build_deb_package.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-#DEB build script for Travis-CI
 #DDCToolbox binary needs to be in working directory
 
-debname="ddctoolbox_"$base_version"-build"$TRAVIS_BUILD_NUMBER
+debname="ddctoolbox_"$1"_linux64"
 echo $debname
 mkdir $debname
 mkdir $debname"/DEBIAN"
@@ -30,7 +29,7 @@ EOT
 
 cat <<EOT >> $debname"/DEBIAN/control"
 Package: ddc-toolbox
-Version: $base_version-$TRAVIS_BUILD_NUMBER
+Version: $1
 Section: sound
 Priority: optional
 Architecture: amd64
