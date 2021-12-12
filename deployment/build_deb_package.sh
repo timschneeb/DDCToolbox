1988c7b00c57f890975e8f50460646bda6e21dd9
Tim Schneeberger <tim.schneeberger@outlook.de>
2021-12-12 23:38:40 +0100

    Update build_deb_package.sh

--- deployment/build_deb_package.sh
+++ deployment/build_deb_package.sh
@@ -8,6 +8,7 @@ mkdir $debname"/DEBIAN"
 mkdir -p $debname"/usr/bin"
 mkdir -p $debname"/usr/share/applications"
 mkdir -p $debname"/usr/share/pixmaps"
+chmod +x "DDCToolbox"
 cp "DDCToolbox" $debname"/usr/bin/ddctoolbox"
 
 cp "img/icon.png" $debname"/usr/share/pixmaps/ddctoolbox.png"
